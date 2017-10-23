734ee0f64042108c146cd47c5bf8c34fbb6e18ab
Dom Smith <dom@hiikey.com>
2017-10-23 00:05:46 -0400

    Initial Commit

---
+++ Sickcall Advisor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sickcall Advisor
+//
+//  Created by Dominic Smith on 10/23/17.
+//  Copyright © 2017 Sickcall LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
